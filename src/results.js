6adfaa5967a341c5ac8aa908a75e2a5fc39736cf
naefdominik <naef.dominik@hotmail.com>
2026-05-26 23:22:04 +0200

    add entry

--- src/results.js
+++ src/results.js
@@ -14,5 +14,6 @@ export const results = [
   { date: '18.05.2026', domi: 3, gaetan: 2 },
   { date: '20.05.2026', domi: 0, gaetan: 3 },
   { date: '21.05.2026', domi: 3, gaetan: 2 },
-  { date: '22.05.2026', domi: 0, gaetan: 3 },
+  { date: '22.05.2026', domi: 0, gaetan: 6 },
+  { date: '26.05.2026', domi: 0, gaetan: 3 },
 ]
